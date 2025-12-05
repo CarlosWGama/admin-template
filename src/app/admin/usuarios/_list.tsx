a50e343ed331a87011d17ce7be79a29ae555a76d
CarlosWGama <carloswgama@gmail.com>
2025-12-05 08:42:42 -0300

    Pagination e Filter

--- src/app/admin/usuarios/_list.tsx
+++ src/app/admin/usuarios/_list.tsx
@@ -2,21 +2,33 @@
 import { useEffect, useState } from "react"
 import UserServices from "@/services/user";
 import Link from "next/link";
-import { AppButton, AppLoader, AppModal } from "@/themes/components";
+import { AppButton, AppInput, AppLoader, AppModal, AppSelect } from "@/themes/components";
 import { getFlashData } from "@/helpers/router";
+import { useSearchParams } from "next/navigation";
 
 export default function UserList() {
 
-    const [ users, setUsers ] = useState<any[]>([]);
-    const [ userRemove, setUserRemove ] = useState<any>(null);
-    const [ success, setSuccess ] = useState<string|null>(null);
-    const [ error, setError ] = useState<string|null>(null);
-    const [ loading, setLoading ] = useState(true);
-    
+    const params = useSearchParams();
+    const [users, setUsers] = useState<any[]>([]);
+    const [userRemove, setUserRemove] = useState<any>(null);
+    const [success, setSuccess] = useState<string | null>(null);
+    const [error, setError] = useState<string | null>(null);
+    const [loading, setLoading] = useState(true);
+    const [page, setPage] = useState(Number(params.get('page') ?? 1));
+    const [pagination, setPagination] = useState<any>(null);
+    const [filter, setFilter] = useState({
+        name: '',
+        email: '',
+        admin: '-1',
+    })
+
     // ======================================================================
-    const getUsers = async () => {
-        const response = await UserServices.getAll();
-        if (response.success) setUsers(response.users);
+    const getUsers = async (page: number) => {
+        const { success, users, pagination } = await UserServices.getAll(page, filter);
+        if (success) {
+            setUsers(users);
+            setPagination(pagination);
+        }
         setLoading(false);
     }
     // -----------
@@ -31,32 +43,55 @@ export default function UserList() {
         setUserRemove(null);
         await UserServices.delete(userRemove.id)
         setSuccess('Usuário excluido com sucesso!');
-        getUsers();
+        getUsers(1);
     }
     // -----------
     const handleModalCancel = async () => {
         setUserRemove(null);
     }
     // -----------
+    const handlePage = async (newPage: number) => {
+        setPage(newPage);
+        getUsers(newPage);
+    }
+    // -----------
     useEffect(() => {
         //Recupera usuário
-        getUsers();
+        getUsers(page);
         //Recupera mensagem 
         (() => {
             const data = getFlashData();
             if (data?.success) setSuccess(data.success);
             if (data?.error) setError(data.error);
         })()
-        
+
     }, []);
     // ======================================================================
     return (
         <>
-            { success && <p className="bg-[#6eef01] px-5 text-center rounded-full color-[white] p-1">{success}</p> }
-            { error && <p className="bg-[tomato] px-5 text-center rounded-full color-[white] p-1">{error}</p> }
-            
-            { loading && <div className="flex justify-center"><AppLoader size={50} className="self-center"/></div>}
-            { !loading &&  <div className="overflow-x-auto">
+            {/* FILTRO [INICIO] */}
+            <h3 className="text-[18px] font-bold">Filtros</h3>
+            <div className="flex flex-col border-b-[2px] border-[#dedede] p-2">
+                <div className="flex gap-2">
+                    <AppInput type="text" label="Nome" value={filter.name} onChange={(e) => setFilter({ ...filter, name: e.target.value })} />
+                    <AppInput type="email" label="Email" value={filter.email} onChange={(e) => setFilter({ ...filter, email: e.target.value })} />
+
+                    <AppSelect label="Admininistrador" value={filter.admin} onChange={(e) => setFilter({ ...filter, admin: e.target.value })}>
+                        <option value="-1">Todos</option>
+                        <option value="1">Sim</option>
+                        <option value="0">Não</option>
+                    </AppSelect>
+
+                </div>
+                <AppButton title="Filtrar" className="w-[100px]" type="outline" onClick={() => getUsers(page)} />
+            </div>
+            {/* FILTRO [FIM] */}
+
+            {success && <p className="bg-[#6eef01] px-5 text-center rounded-full color-[white] p-1">{success}</p>}
+            {error && <p className="bg-[tomato] px-5 text-center rounded-full color-[white] p-1">{error}</p>}
+
+            {loading && <div className="flex justify-center"><AppLoader size={50} className="self-center" /></div>}
+            {!loading && <div className="overflow-x-auto">
                 <table className="min-w-full bg-white">
                     {/* HEADER  */}
                     <thead>
@@ -75,7 +110,7 @@ export default function UserList() {
                                 <td className="py-2 px-4 border-b border-gray-200 text-sm">{user.name}</td>
                                 <td className="py-2 px-4 border-b border-gray-200 text-sm">{user.email}</td>
                                 <td className="py-2 px-4 border-b border-gray-200 text-sm">{user.admin ? 'ADMININISTRADOR' : 'USUÁRIO'}</td>
-                                
+
                                 <td className="py-2 px-4 border-b border-gray-200 text-sm">
                                     <Link href={`/admin/usuarios/editar/${user.id}`}>
                                         <i className="ion-edit text-[20px] text-[#1aab67] mx-[10px] cursor-pointer" />
@@ -86,16 +121,21 @@ export default function UserList() {
                         ))}
                     </tbody>
                 </table>
+
+                {pagination && <div className="flex justify-end mt-[20px]">
+                    {!pagination.firstPage && <AppButton title="Anterior" className="mr-[10px]" icon="arrow-left-a" form="round" onClick={() => handlePage(page - 1)} />}
+                    {!pagination.lastPage && <AppButton title="Proximo" className="ml-[10px]" icon="arrow-right-a" form="round" onClick={() => handlePage(page + 1)} />}
+                </div>}
             </div>}
 
             {userRemove && <AppModal title="Remover usuário">
                 <p>Deseja realmente remover o usuário {userRemove.name} ({userRemove.email})?</p>
                 <div className="flex justify-between p-[20px]">
-                    <AppButton title="Sim" icon="checkmark" form="round" color="#428f01" onClick={handleModalConfirm}/>
-                    <AppButton title="Cancelar" icon="close" color="tomato" form="round" onClick={handleModalCancel}/>
+                    <AppButton title="Sim" icon="checkmark" form="round" color="#428f01" onClick={handleModalConfirm} />
+                    <AppButton title="Cancelar" icon="close" color="tomato" form="round" onClick={handleModalCancel} />
                 </div>
 
-            </AppModal>}  
+            </AppModal>}
         </>
     )
 }
--- src/services/user.ts
+++ src/services/user.ts
@@ -4,16 +4,16 @@ import Cookies from 'js-cookie';
  * SERVICES DE GERENCIAMENTO DE USUÁRIOS
  */
 const UserServices = {
-    
+
     /**
      * Autenticação de usuário
      */
-    login: async (email:string, password:string): Promise<{success: boolean, message?: string}> => {
+    login: async (email: string, password: string): Promise<{ success: boolean, message?: string }> => {
         //Simula um atrasado de 1seg
         await new Promise((resolve) => setInterval(resolve, 1000));
-        
+
         if (email == 'teste@teste.com' && password == '123456') {
-            const user = {email, id: 1, name: 'Teste'};
+            const user = { email, id: 1, name: 'Teste' };
             Cookies.set('user', JSON.stringify(user));
             return { success: true }
         } else
@@ -24,8 +24,8 @@ const UserServices = {
      * Retorna o usuário autenticado
      * @returns 
      */
-    getCurrentUser: () => {        
-        const user =  Cookies.get('user');
+    getCurrentUser: () => {
+        const user = Cookies.get('user');
         return user ? JSON.parse(user) : null;
     },
 
@@ -41,7 +41,7 @@ const UserServices = {
     /**
      * Remove todos os vinculos do usuário com o back-end
      */
-    logout: async (): Promise<{success: boolean}> => {
+    logout: async (): Promise<{ success: boolean }> => {
         //Simula um atrasado de 1seg
         new Promise((resolve) => setInterval(resolve, 1000));
         Cookies.remove('user');
@@ -52,26 +52,33 @@ const UserServices = {
      * Retorna todos os usuários do sistema
      * @returns 
      */
-    getAll: async(): Promise<{success: boolean, users?: any[]}>  => {
-        new Promise((resolve) => setInterval(resolve, 2000));
+    getAll: async (page: number, filter: any = {}): Promise<{ success: boolean, users?: any[], pagination?: any }> => {
+        await new Promise((resolve) => setInterval(resolve, 2000));
 
-        return {success: true, users: [
-            {id: 1, name: 'Teste', email: 'teste@teste.com', admin: false},
-            {id: 2, name: 'Admin', email: 'admin@teste.com', admin: true},
-            {id: 3, name: 'Carlos', email: 'carlos@teste.com', admin: false}
-        ]}
+        return {
+            success: true, users: [
+                { id: 1, name: 'Teste', email: 'teste@teste.com', admin: false },
+                { id: 2, name: 'Admin', email: 'admin@teste.com', admin: true },
+                { id: 3, name: 'Carlos', email: 'carlos@teste.com', admin: false }
+            ],
+            pagination: {
+                firstPage: false,
+                lastPage: false,
+            }
+
+        }
     },
 
     /**
      * Retorna dados de um unico usuário
      * @returns 
      */
-    getById: async(id: number): Promise<{success: boolean, user: any}>  => {
+    getById: async (id: number): Promise<{ success: boolean, user: any }> => {
         new Promise((resolve) => setInterval(resolve, 1000));
         if (id == 1)
-            return {success: true, user: {id: 1, name: 'Teste', email: 'teste@teste.com', admin: false}}
-        else    
-            return {success: false, user: null}
+            return { success: true, user: { id: 1, name: 'Teste', email: 'teste@teste.com', admin: false } }
+        else
+            return { success: false, user: null }
     },
 
     /**
@@ -79,9 +86,9 @@ const UserServices = {
      * @param user 
      * @returns 
      */
-    create: async (user:any): Promise<{success: boolean, error?: string}> => {
-       new Promise((resolve) => setInterval(resolve, 1000));
-       return { success: true};
+    create: async (user: any): Promise<{ success: boolean, error?: string }> => {
+        new Promise((resolve) => setInterval(resolve, 1000));
+        return { success: true };
     },
 
     /**
@@ -89,9 +96,9 @@ const UserServices = {
      * @param user 
      * @returns 
      */
-    update: async (user:any): Promise<{success: boolean, error?: string}> => {
-       new Promise((resolve) => setInterval(resolve, 1000));
-       return { success: true};
+    update: async (user: any): Promise<{ success: boolean, error?: string }> => {
+        new Promise((resolve) => setInterval(resolve, 1000));
+        return { success: true };
     },
 
     /**
@@ -99,9 +106,9 @@ const UserServices = {
      * @param user 
      * @returns 
      */
-    delete: async (id: number): Promise<{success: boolean}> => {
-       new Promise((resolve) => setInterval(resolve, 1000));
-       return { success: true};
+    delete: async (id: number): Promise<{ success: boolean }> => {
+        new Promise((resolve) => setInterval(resolve, 1000));
+        return { success: true };
     }
 
 
